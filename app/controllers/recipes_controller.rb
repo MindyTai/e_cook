8067de26c737d6da5b32d7e3de2b02b8b90e2016
Mindy <mindytai175@gmail.com>
2019-12-28 17:00:15 +0800

    Day4

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
-
-  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_recipe, only: [ :edit, :update, :destroy]
+  
   def index
     @recipes = Recipe.all
   end
@@ -11,10 +12,13 @@ class RecipesController < ApplicationController
 
   def show
     # find_recipe
+    @recipe = Recipe.find(params[:id])
   end
 
   def edit
     # find_recipe
+    # @recipe= Recipe.find_by(user_id: current_user.id, id: params[:id])
+    
   end
 
   def update
@@ -39,7 +43,10 @@ class RecipesController < ApplicationController
 
   def create
     #strong parameters
-    @recipe = Recipe.new(clean_params)
+    # @recipe = Recipe.new(clean_params)
+    # @recipe.user_id = current_user.id
+
+    @recipe = current_user.recipes.new(clean_params)
     if @recipe.save #write to database
       flash["notice"] = '新增成功'
       redirect_to "/"
@@ -48,12 +55,17 @@ class RecipesController < ApplicationController
     end
   end
 
+  def my
+    @recipes = current_user.recipes
+    render :index
+  end
+
   private
     def clean_params
       params.require(:recipe).permit(:title, :description, :photo)
     end
 
     def find_recipe
-      @recipe = Recipe.find(params[:id])
+      @recipe = current_user.recipes.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get "/", to: "welcome#home"
+  # resources :users, only:[] do
+  #   collection do
+  #     get :favorites
+  #   end
+  # end
+  
   root "recipes#index"
   get "/other", to: "recipes#other"
-  resources :recipes
+  resources :recipes do
+    collection do
+      get :my
+    end
+  end
 end
